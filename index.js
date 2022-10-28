b09c76144726bc8b383d912425bda8e2e0657a5e
CristianaMartins <cristianamartinsfigueiredo@gmail.com>
2022-10-28 16:22:53 +0100

    Add the description of the card

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ function selectCard() {
   // get the random card from the list
   const randomCard = tarotData[randomResult];
   console.log(randomCard);
-  //modify html with the new image of the card
-  // modify html with a new image for the text (meaning of the card)
+  // modify html with the new image of the card
+  // get an element by id
+  var element = document.getElementById("cardPlaceholder");
 
-  window.alert("olá");
+  // cleans the inside of the component
+  element.innerHTML = "";
+
+  // adds the element to the html node
+  const before = `<img
+  class="card-image"
+  src="${randomCard.img}"
+  />`;
+  element.insertAdjacentHTML("beforeend", before);
+  // modify html with a new text (meaning of the card)
+  // get an element by id
+  var element2 = document.getElementById("captionPlaceholder");
+
+  // cleans the inside of the component
+  element2.innerHTML = "";
+
+  // adds the element to the html node
+  const before2 = `<h3>Meaning:</h3><h4>${randomCard.upright}</h4>`;
+  element2.insertAdjacentHTML("beforeend", before2);
 }
