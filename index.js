08f3fcaaa7dad498a722f957839607799393b064
CristianaMartins <cristianamartinsfigueiredo@gmail.com>
2022-10-28 15:30:26 +0100

    changed data select random card

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-console.log("hello World");
+console.log(tarotData);
 function selectCard() {
+  // random number bettwen 0 and list.length
+  const randomResult = Math.floor(Math.random() * tarotData.length);
+  // get the random card from the list
+  const randomCard = tarotData[randomResult];
+  console.log(randomCard);
+  //modify html with the new image of the card
+  // modify html with a new image for the text (meaning of the card)
+
   window.alert("olá");
 }
